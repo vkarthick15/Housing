ba4e1d37a519cfee6405adfe58fe0aa8b2320377
vkarthick15 <37335644+vkarthick15@users.noreply.github.com>
2022-07-03 17:39:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
-model = pickle.load(open("housing.pkl", 'rb'))
+model = pickle.load(open("housingprice.pkl", 'rb'))
 
 #Header
 st.title("Housing Price Prediction")
